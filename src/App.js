f5ab80bd1b09c7d96c7f9e94e6661a587d67d303
Jesse <jesse.chen26@myhunter.cuny.edu>
2025-04-07 14:09:19 -0400

    changes for github pages to work

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
-      <Router basename="/bank-of-react-starter-code">
+      <Router basename="A3">
         <div>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
